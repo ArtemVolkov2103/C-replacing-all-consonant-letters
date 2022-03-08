e20442c20b62f381d5bd67a172c6defeb6f5863b
ArtemVolkov2103 <82831826+ArtemVolkov2103@users.noreply.github.com>
2022-03-08 13:00:57 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace Praktika_WinForms_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(448, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(424, 469);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 526);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem indexToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Praktika_WinForms_4
+{
+    public partial class Form1 : Form
+    {
+        ToolStripMenuItem fileItem = new ToolStripMenuItem("Файл");
+        ToolStripMenuItem transform = new ToolStripMenuItem("Преобразовать");
+        ToolStripMenuItem openItem = new ToolStripMenuItem("Открыть");
+        ToolStripMenuItem saveItem = new ToolStripMenuItem("Сохранить");
+        ToolStripMenuItem saveAsItem = new ToolStripMenuItem("Сохранить как");
+        ToolStripMenuItem exit = new ToolStripMenuItem("Выход");
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            fileItem.DropDownItems.Add(openItem);
+            fileItem.DropDownItems.Add(saveItem);
+            fileItem.DropDownItems.Add(saveAsItem);
+            fileItem.DropDownItems.Add(exit);
+
+            menuStrip1.Items.Add(fileItem);
+            menuStrip1.Items.Add(transform);
+            saveItem.Enabled = false;
+            saveAsItem.Enabled = false;
+
+            transform.Click += Transform_Click;
+            openItem.Click += openItem_Click;
+            saveItem.Click += saveItem_Click;
+            saveAsItem.Click += saveAsItem_Click;
+            exit.Click += exit_Click;
+        }
+
+        private void Transform_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            string[] sogl = "б в г д ж з й к л м н п р с т ф х ц ч ш щ".Split(' ');
+            for (int i = 0; i < fileText.Count; i++)
+            {
+                string str = fileText[i];
+                bool IsUpperCase = false;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    for (int k = 0; k < sogl.Length; k++)
+                    {
+                        if (str[j].ToString().Contains(sogl[k]))
+                        {
+                            textBox1.Text += str[j].ToString().ToUpper();
+                            IsUpperCase = true;
+                            break;
+                        }
+                        else IsUpperCase = false;
+                    }
+                    if (!IsUpperCase)
+                        textBox1.Text += str[j].ToString();
+                }
+            }
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        List<string> fileText = new List<string>();
+        List<string> fileTextTransformed = new List<string>();
+        string filePath = "";
+        private void openItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.FileName = "Document";
+            ofd.DefaultExt = ".txt";
+            ofd.Filter = "Текстовый файл. Файл формата .txt|*.txt";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+            }
+            string line;
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                textBox1.Clear();
+                fileText.Clear();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fileText.Add(line);
+                    fileTextTransformed.Add(line);
+                    textBox1.Text += line;
+                }
+            }
+            saveItem.Enabled = true;
+            saveAsItem.Enabled = true;
+        }
+        private void saveItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, false))//false=перезапись
+                {
+                    foreach (string item in fileTextTransformed)
+                        writer.Write(item);
+                }
+                MessageBox.Show("Файл сохранен");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка записи");
+            }
+        }
+        private void saveAsItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter SW;
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Отмена");
+                return;
+            }
+            try
+            {
+                // получаем выбранный файл
+                sfd.DefaultExt = ".txt";
+                sfd.Filter = "Текстовый файл.Файл формата .txt | *.txt";
+                string filename = sfd.FileName;
+
+                // сохраняем текст в файл
+                SW = new StreamWriter(sfd.FileName + ".txt");
+                SW.Write(textBox1.Text);
+                SW.Close();
+                MessageBox.Show("Файл сохранен");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка записи");
+            }
+        }
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
